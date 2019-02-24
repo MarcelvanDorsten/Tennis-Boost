dba66402242abef6ae292420f94d3227aaf4870b
MarcelvanDorsten <info@asdonline.nl>
2019-02-24 22:33:59 +0100

    VerhuurComponent Part 2

--- src/app/verhuur/verhuur.component.ts
+++ src/app/verhuur/verhuur.component.ts
@@ -2,6 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 import { Tenniscannon } from '../shared/tenniskanon';
 
+const TENNISCANNONS: Tenniscannon[] = [
+  {
+    id: '0',
+    naam: 'Tennis Twist',
+    foto: '/assets/twist.jpg',
+    omschrijving: 'Tennis Twist® is speciaal ontworpen voor beginnende tennisspelers. Het compacte formaat en de eenvoudige bediening maken het een ideale eerste balmachine voor kinderen die net beginnen met tennissen. Als een kind een racket kan slingeren, kan hij of zij met Tennis Twist spelen.'
+  },
+  {
+    id: '1',
+    naam: 'Tennis Cube',
+    foto: '/assets/cube.jpg',
+    omschrijving: 'De lichtste, meest compacte tennismachine die beschikbaar is voor beginnende en gevorderde spelers.'
+  }
+  ];
+
 @Component({
   selector: 'app-verhuur',
   templateUrl: './verhuur.component.html',
@@ -9,20 +24,8 @@ import { Tenniscannon } from '../shared/tenniskanon';
 })
 export class VerhuurComponent implements OnInit {
 
-  tenniscannons: Tenniscannon[] = [
-    {
-      id: '0',
-      naam: 'Tennis Twist',
-      foto: '/assets/twist.jpg',
-      omschrijving: 'Tennis Twist® is speciaal ontworpen voor beginnende tennisspelers. Het compacte formaat en de eenvoudige bediening maken het een ideale eerste balmachine voor kinderen die net beginnen met tennissen. Als een kind een racket kan slingeren, kan hij of zij met Tennis Twist spelen.'
-    },
-    {
-      id: '1',
-      naam: 'Tennis Cube',
-      foto: '/assets/cube.jpg',
-      omschrijving: 'De lichtste, meest compacte tennismachine die beschikbaar is voor beginnende en gevorderde spelers.'
-    }
-    ];
+  tenniscannons = TENNISCANNONS;
+  selectedTenniscannon = TENNISCANNONS[0];
 
   constructor() { }
 
