4228b612e96d679d8fa13a71781ea49ce6f2a07f
MarcelvanDorsten <info@asdonline.nl>
2019-03-02 22:28:06 +0100

    Angular Router Basics

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { VerhuurComponent } from '../verhuur/verhuur.component';
+import { DetailsComponent } from '../details/details.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'verhuur', component: VerhuurComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+
+];
