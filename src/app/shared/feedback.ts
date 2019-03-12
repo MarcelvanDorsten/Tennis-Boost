d0c158e4f67564acb4fda490f0523d82f36bc525
MarcelvanDorsten <info@asdonline.nl>
2019-03-12 23:22:35 +0100

    Reactive Forms Part 1

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback {
+    voornaam: string;
+    achternaam: string;
+    telnum: number;
+    email: string;
+    accoord: boolean;
+    contacttype: string;
+    boodschap: string;
+};
+
+export const ContactType = ['None', 'Tel', 'Email'];
